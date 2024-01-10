92bb65acadeac6378b62f1ab665c03c60eb77dda
hyunjunhwang1994 <hyunjunhwang1994@gmail.com>
2024-01-10 22:24:54 +0900

    feat: node.js nest.js

---
+++ nestjs_server/src/app.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get() // Root Page
+  getHome() {
+    return 'Home Page'
+  }
+
+  @Get("post")
+  getPost() {
+    return "Post Page"
+  }
+
+  @Get("user")
+  getUser(){
+    return "User Page"
+  }
+
+}
